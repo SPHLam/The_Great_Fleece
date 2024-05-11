6f609a70a60d389dc18eda7ec180dd1eddc8a547
SPHLam <samphanhoailam@gmail.com>
2024-05-11 12:01:39 +0700

    Animate Security Cameras
    - Turn the camera's color to red when spot the Player and then freeze the camera

--- Assets/The Great Fleece/Game/Scripts/SecurityCamera.cs
+++ Assets/The Great Fleece/Game/Scripts/SecurityCamera.cs
@@ -4,12 +4,27 @@ using UnityEngine;
 
 public class SecurityCamera : MonoBehaviour
 {
+    private MeshRenderer _meshRenderer;
     public GameObject gameOverCutscene;
+    private Animator _animator;
+
+    void Start()
+    {
+        _meshRenderer = GetComponent<MeshRenderer>();
+        _animator = transform.parent.gameObject.GetComponent<Animator>();
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
+            _meshRenderer.material.SetColor("_TintColor", new Color(0.6f, 0.11f, 0.11f, 0.0225f));
+            StartCoroutine(freezeCamera());
             gameOverCutscene.SetActive(true);
         }
+    }
+    IEnumerator freezeCamera()
+    {
+        _animator.enabled = false;
+        yield return new WaitForSeconds(0.5f);
     }
 }
